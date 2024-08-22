e36baf3ab76fbd392119126aac05db35da4b4114
SURAJ_KUMAR <surajk38656@gmail.com>
2024-08-22 12:18:33 +0530

    Check Palindrom or Not

--- src/check_palindrome/MyMain.java
+++ src/check_palindrome/MyMain.java
@@ -1,13 +1,24 @@
 package check_palindrome;
-
 import java.util.Scanner;
 
 public class MyMain {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the String : ");
+        System.out.print("Enter the String : ");
         String string = scanner.nextLine();
         Palindrome palindrome = new Palindrome();
-        System.out.println(palindrome.isPalindrome(string));
+        System.out.println("palindrome = " +palindrome.isPalindrome(string));
+
+        System.out.print("Enter the Integer : ");
+        int integer = scanner.nextInt();
+        Palindrome palindrome1 = new Palindrome();
+        System.out.println("palindrome = " + palindrome1.isPalindromInteger(integer));
     }
 }
+
+/*
+Enter the String : oppo
+palindrome = true
+Enter the Integer : 78987
+palindrome = true
+*/
--- src/check_palindrome/Palindrome.java
+++ src/check_palindrome/Palindrome.java
@@ -7,7 +7,23 @@ public class Palindrome {
         for(int i=string.length()-1; i>=0; i--){
             rev.append(string.charAt(i));
         }
-        if(rev.toString().equals(string)){
+        // It ignores the lower or upper case.
+        if(rev.toString().equalsIgnoreCase(string)){
+            response = true;
+        }
+        return response;
+    }
+    // logic of check number is palindrome or not !
+    public boolean isPalindromInteger(int n){
+        int rev=0;
+        int temp = n;
+        boolean response = false;
+        while (n!=0){
+            int remain = n%10;
+            rev = rev*10+remain;
+            n/=10;
+        }
+        if(temp==rev){
             response = true;
         }
         return response;
