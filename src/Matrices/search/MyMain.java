f6197067c1e045b8ccfa7697c416b47aae8a7493
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-08 11:58:01 +0530

    Search element int matrix

--- src/Matrices/search/MyMain.java
+++ src/Matrices/search/MyMain.java
@@ -1,2 +1,22 @@
-package Matrices.search;public class MyMain {
+package Matrices.search;
+import java.util.Scanner;
+
+public class MyMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of row : ");
+        int row = scanner.nextInt();
+        System.out.print("Enter the number of column : ");
+        int col = scanner.nextInt();
+        int[][] matrices = new int[row][col];
+        System.out.println("Enter the "+row*col+" elements : ");
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                matrices[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.print("Enter the search element : ");
+        int search = scanner.nextInt();
+        Search.searchElement(matrices,row,col,search);
+    }
 }
--- src/Matrices/search/Search.java
+++ src/Matrices/search/Search.java
@@ -1,2 +1,13 @@
-package Matrices.search;public class Search {
+package Matrices.search;
+
+public class Search {
+    public static void searchElement(int[][] matrices,int row, int col,int search){
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(search==matrices[i][j]){
+                    System.out.println("element found at index : "+ "matrices["+i+","+j+"]");
+                }
+            }
+        }
+    }
 }
