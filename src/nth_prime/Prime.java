81532948ed0c3b80333bde3ff3251774e2ddabe0
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-08 17:54:54 +0530

    nthPrime

--- src/nth_prime/Prime.java
+++ src/nth_prime/Prime.java
@@ -5,7 +5,7 @@ public class Prime {
         if(n==0 || n==1){
             return false;
         }
-        for(int i=2; i<n; i++){
+        for(int i=2; i<=n/2; i++){
             if(n%i==0){
                 return false;
             }
